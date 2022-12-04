61babeda8bb08575b7975106f014612678dd7ee2
Alex <alexsa-8@mail.ru>
2022-12-04 17:00:51 +0300

    реализация ArrayList

---
+++ src/main/java/com/skypro/realizationarraylist/RealizationArrayListApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.realizationarraylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealizationArrayListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealizationArrayListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/realizationarraylist/RealizationArrayListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.realizationarraylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealizationArrayListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
